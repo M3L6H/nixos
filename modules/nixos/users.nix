8d47ea31a23271b568ec7176c6d3f5287ca503f1
m3l6h <sisoac@hotmail.com>
2024-07-17 16:33:00 -0500

    Make zsh default shell

--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,12 +1,20 @@
 { config, lib, pkgs, ... }: {
   options = {
     users.enable = lib.mkEnableOption "enables users module";
+    users.zsh.enable = lib.mkEnableOption "enables zsh as default shell";
   };
 
   config = lib.mkIf config.users.enable {
+    users.zsh.enable = lib.mkDefault true;
+
     environment.systemPackages = with pkgs; [
       home-manager
     ];
+
+    programs.zsh.enable = config.users.zsh.enable;
+
+    # Completion for system packages (e.g. systemd)
+    environment.pathsToLink = lib.mkIf config.users.zsh.enable [ "/share/zsh" ];
 
     users.users.m3l6h = {
       isNormalUser = true;
@@ -15,6 +23,7 @@
       packages = with pkgs; [
         kdePackages.kate
       ];
+      shell = lib.mkIf config.users.zsh.enable pkgs.zsh;
     };
   };
 }
