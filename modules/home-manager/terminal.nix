8e05f873dad511c5ce8d8402c7994ceec77cddb2
m3l6h <sisoac@hotmail.com>
2024-07-18 08:49:53 -0500

    Fix Kitty config

--- modules/home-manager/terminal.nix
+++ modules/home-manager/terminal.nix
@@ -4,8 +4,18 @@
   };
 
   config = lib.mkIf config.terminal.enable {
-    home.packages = with pkgs; [
-      kitty
-    ];
+    programs.kitty = {
+      enable = true;
+      font = {
+        package = (pkgs.nerdfonts.override { fonts = [ "VictorMono" ]; });
+	name = "VictorMono Nerd Font";
+      };
+      settings = {
+        enable_audio_bell = false;
+	confirm_os_window_close = 0;
+	tab_bar_edge = "top";
+      };
+      theme = "Tokyo Night";
+    };
   };
 }
