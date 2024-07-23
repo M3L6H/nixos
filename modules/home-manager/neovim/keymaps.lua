d750721cf41f9e5ad3619ce59f7dd43d735d3dbb
m3l6h <sisoac@hotmail.com>
2024-07-23 06:52:06 -0500

    Add some keymaps
    - Add move lines in visual mode
    - Add better joining and paging
    - Add navigation in insert mode

--- modules/home-manager/neovim/keymaps.lua
+++ modules/home-manager/neovim/keymaps.lua
@@ -3,5 +3,19 @@ vim.g.maplocalleader = " "
 
 local km = vim.keymap
 
--- km.set("n", "<leader>pv", vim.cmd.Ex)
+-- Move lines in visual mode
+km.set("v", "J", ":m '>+1<CR>gv=gv")
+km.set("v", "K", ":m '<-2<CR>gv=gv")
+
+km.set("n", "J", "mzJ`z")
+km.set("n", "<C-d>", "<C-d>zz")
+km.set("n", "<C-u>", "<C-u>zz")
+km.set("n", "n", "nzzzv")
+km.set("n", "N", "Nzzzv")
+
+-- navigate in insert mode
+km.set("i", "<A-h>", "<Left>")
+km.set("i", "<A-l>", "<Right>")
+km.set("i", "<A-j>", "<C-o>gj")
+km.set("i", "<A-k>", "<C-o>gk")
 
