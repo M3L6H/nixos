9914338b7556392bdfedbfc89072d5042ee0cc41
m3l6h <8094643+M3L6H@users.noreply.github.com>
2024-08-14 07:29:46 -0500

    Use absolute paths in aliases

--- modules/home-manager/aliases.nix
+++ modules/home-manager/aliases.nix
@@ -3,10 +3,10 @@
     arctis = "wpctl set-default \"$(wpctl status -n | grep -zoP '(?<=Sinks:)(?s).*?(?=├─)' | grep -a 'stereo-game' | awk '{print ($2+0)}')\"; pactl set-sink-volume @DEFAULT_SINK@ 100%";
     gds = "git diff --staged";
     gs = "git status";
-    hms = "home-manager switch --flake .#m3l6h";
+    hms = "home-manager switch --flake /etc/nixos#m3l6h";
     icat = "kitten icat";
     mntimp = "sudo mkdir /mnt; sudo mount -o subvol=/ /dev/mapper/root /mnt";
-    nxs = "sudo nixos-rebuild switch --flake .#nixos";
+    nxs = "sudo nixos-rebuild switch --flake /etc/nixos#nixos";
   };
 }
 
