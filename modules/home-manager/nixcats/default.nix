11c8b1ad904550ed54f5fdb4ab58aeab9f1818dc
m3l6h <8094643+M3L6H@users.noreply.github.com>
2025-04-21 16:52:52 -0500

    Use lazy with nixcats

--- modules/home-manager/nixcats/default.nix
+++ modules/home-manager/nixcats/default.nix
@@ -1,17 +1,17 @@
 { config, lib, inputs, ... }: let
   utils = inputs.nixCats.utils;
 in {
-  imports = [
-    inputs.nixCats.homeModule
-  ];
-
   options = {
     nixcats.enable = lib.mkEnableOption "enables nixcats-nvim";
   };
 
-  config = {
+  imports = [
+    inputs.nixCats.homeModule
+  ];
+
+  config = lib.mkIf config.nixcats.enable {
     nixCats = {
-      enable = config.nixcats.enable;
+      enable = true;
 
       addOverlays = [
         (utils.standardPluginOverlay inputs)
@@ -24,37 +24,42 @@ in {
       categoryDefinitions.replace = ({ pkgs, settings, categories, extra, name, mkPlugin, ... }@packageDef: {
         lspsAndRuntimeDeps = {
           lua = with pkgs; [
+            fzf
             lua-language-server
+            nil
+            ripgrep
             stylua
           ];
         };
+
+        startupPlugins = {
+          general = with pkgs.vimPlugins; [
+            lazy-nvim
+            nvim-treesitter.withAllGrammars
+            which-key-nvim
+          ];
+        };
+
+        # Empty b/c we are using Lazy.nvim for lazy loading
+        optionalPlugins = {};
       });
 
       packageDefinitions.replace = {
         neovim = {pkgs, name, ... }: {
-          # they contain a settings set defined above
-          # see :help nixCats.flake.outputs.settings
           settings = {
             suffix-path = true;
             suffix-LD = true;
             wrapRc = true;
-            # unwrappedCfgPath = "/path/to/here";
-            # IMPORTANT:
-            # your alias may not conflict with your other packages.
-            aliases = [ "nixcats" ];
-            # neovim-unwrapped = inputs.neovim-nightly-overlay.packages.${pkgs.system}.neovim;
+            aliases = [ "v" "vi" "vim" "nvim" ];
             hosts.python3.enable = true;
             hosts.node.enable = true;
+            unwrappedCfgPath = builtins.toString ./.;
           };
-          # and a set of categories that you want
-          # (and other information to pass to lua)
-          # and a set of categories that you want
           categories = {
             general = true;
             lua = true;
             nix = true;
           };
-          # anything else to pass and grab in lua with `nixCats.extra`
           extra = {
             nixdExtras.nixpkgs = ''import ${pkgs.path} {}'';
           };
--- modules/home-manager/nixcats/init.lua
+++ modules/home-manager/nixcats/init.lua
@@ -1,15 +1,3 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
--- Set highlight on search
-vim.opt.hlsearch = true
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
--- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+require("opts")
+require("keymaps")
+require("lazy-setup")
