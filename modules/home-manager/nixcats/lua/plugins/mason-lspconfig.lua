e6f9ad8a8f807558d6a3f93ac8552e1e825f8a23
m3l6h <8094643+M3L6H@users.noreply.github.com>
2025-04-29 19:12:37 -0500

    Add scroll, statuscolumn, & words. Configure picker keymaps
    Also:
    - Clean up redundant keymaps in Mason

--- modules/home-manager/nixcats/lua/plugins/mason-lspconfig.lua
+++ modules/home-manager/nixcats/lua/plugins/mason-lspconfig.lua
@@ -2,7 +2,7 @@ local M = {
   "williamboman/mason-lspconfig.nvim",
   -- Disable Mason in a Nix environment
   -- Nix serves as the package manager
-  enabled = require("../nixCatsUtils").lazyAdd(true, false),
+  enabled = require("nixCatsUtils").lazyAdd(true, false),
   event = "BufReadPre",
   dependencies = {
     { "williamboman/mason.nvim" },
@@ -29,23 +29,6 @@ local M = {
       "zls",
     },
     handlers = {
-      function(server_name) -- default handler
-        require("lspconfig")[server_name].setup({
-          on_attach = function(ev)
-            local km = vim.keymap
-
-            local opts = { buffer = ev.buf, noremap = true, silent = true }
-            -- km.set("n", "gD", vim.lsp.buf.declaration, opts)
-            -- km.set("n", "gd", vim.lsp.buf.definition, opts)
-            -- km.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
-            -- km.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", opts)
-            -- km.set("n", "K", vim.lsp.buf.hover, opts)
-            -- km.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-            -- km.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-            -- km.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
-          end,
-        })
-      end,
       ["lua_ls"] = function ()
         local lspconfig = require("lspconfig")
         lspconfig.lua_ls.setup {
--- modules/home-manager/nixcats/lua/plugins/oil.lua
+++ modules/home-manager/nixcats/lua/plugins/oil.lua
@@ -50,6 +50,17 @@ local M = {
           oil.open_preview()
         end
       end),
+      desc = "Open preview on oil enter",
+    })
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilActionsPost",
+      callback = function(event)
+        if Snacks ~= nil and event.data.actions.type == "move" then
+          Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+        end
+      end,
+      desc = "Notify lsp on rename using Snacks.nvim",
     })
   end,
 }
