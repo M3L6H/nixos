0026df4518b35a375c540bd9b2b526d9b366b94e
m3l6h <8094643+M3L6H@users.noreply.github.com>
2025-05-14 06:44:56 -0500

    Add inlay hints and code action keymap
    Also:
    - Disable demicolon b/c it conflicts with flash :(

--- modules/home-manager/nixcats/lua/plugins/demicolon.lua
+++ modules/home-manager/nixcats/lua/plugins/demicolon.lua
@@ -1,4 +1,5 @@
 local M = {
+  enabled = false,
   "mawkler/demicolon.nvim",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
--- modules/home-manager/nixcats/lua/plugins/flash.lua
+++ modules/home-manager/nixcats/lua/plugins/flash.lua
@@ -2,6 +2,7 @@ local M = {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
+  ---@diagnostic disable-next-line missing-fields
   opts = {
     jump = {
       nohlsearch = true,
